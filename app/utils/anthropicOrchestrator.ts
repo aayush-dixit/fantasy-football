48b2bc50c7da01b19b4fe50fb9cd778819446db5
aayushdixit <70540219+aayushdixit@users.noreply.github.com>
2024-12-27 21:54:22 -0500

    formatting for ai prompt

--- app/utils/anthropicOrchestrator.ts
+++ app/utils/anthropicOrchestrator.ts
@@ -8,7 +8,7 @@ function generateRecPrompt<T>(
   userTeam: Record<string, filteredPlayer[]>,
   chunk: Record<string, T>
 ) {
-  return `For each team here, please recommend a trade with up to 3 players on each side that would make sense for this user's team based on where the user team has weak positions based on the average position value for a given position. Only return trades where the difference between the value on the user sides and the other team of the trade is less than 500: ${JSON.stringify(userTeam)}. Other teams: ${JSON.stringify(chunk)}. In the response only return the following JSON object:
+  return `For each team here, please recommend a trade with up to 3 players on each side that would make sense for this user's team. Only return trades if a trade can be made where the difference between the value on the user sides and the other team of the trade is less than 500: ${JSON.stringify(userTeam)}. Other teams: ${JSON.stringify(chunk)}. In the response only return the following JSON object:
   {
     "userTeam": {
       "sends": [
@@ -20,6 +20,7 @@ function generateRecPrompt<T>(
       ]
     },
     "otherTeam": {
+      "teamId": [id],
       "sends": [
         {
         "playerName": [playerName],
@@ -33,29 +34,34 @@ function generateRecPrompt<T>(
 }
 
 function generateFinalRecPrompt(allRecs: string[]) {
-  return `Out of all these trade recommendations: ${JSON.stringify(allRecs)}, pick the 3 with the lowest value differences between the user and other team. In the response only return the following JSON object: 
-  {
-    "userTeam": {
-      "sends": [
-        {
-        "playerName": [playerName],
-        "position": [position],
-        "value": [value],
-      }, ...
-      ],
-      "rationale": [rationale]
-    },
-    "otherTeam": {
-      "sends": [
-        {
-        "playerName": [playerName],
-        "position": [position],
-        "value": [value],
-      }, ...
-      ],
-      "rationale": [rationale]
-    },
-  }   `;
+  return `Out of all these trade recommendations: ${JSON.stringify(allRecs)}, pick the 3 with the lowest value differences between the user and other team. Return the response in the following JSON format: 
+  {"suggestions":
+    [
+      {
+        "userTeam": {
+          "sends": [
+            {
+            "playerName": [playerName],
+            "position": [position],
+            "value": [value],
+          }, ...
+          ],
+          "rationale": [rationale]
+        },
+        "otherTeam": {
+          "teamId": [id],
+          "sends": [
+            {
+            "playerName": [playerName],
+            "position": [position],
+            "value": [value],
+          }, ...
+          ],
+          "rationale": [rationale]
+      },...
+    ]
+  }
+}   `;
 }
 
 export async function anthropicOrchestrator(
